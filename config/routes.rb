357de642fc09198565429080ee92f71692d4c9d5
Bradley Tucker <bradley_tucker@hotmail.co.uk>
2022-11-11 16:43:38 +1100

    Stupid Coaching

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get root to: "questions#ask" #root to:
+
+  get "answer", to: "questions#answer"
+
+  get "coach_answer", to: "questions#coach_answer"
 end
